7d0e7b1ee6c6a6d1cd026ae26b6f68daed6f9195
EnriqueMunozc <23040114@alumno.utc.edu.mx>
2026-06-30 08:30:14 -0600

    Configura rutas y primera vista del proyecto

--- practica_equipo/loginproject/settings.py
+++ practica_equipo/loginproject/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Apps del proyecto
+    'usuarios',
+    'inventario',
+    'empleados',
+    'asignaciones',
+    'eventos',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,7 @@ ROOT_URLCONF = 'loginproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
