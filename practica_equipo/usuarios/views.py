37bb3675a48c097307485968a47cb1df4fee6a5c
EnriqueMunozc <23040114@alumno.utc.edu.mx>
2026-06-30 14:55:22 -0600

    dashboards actualizados

--- practica_equipo/usuarios/views.py
+++ practica_equipo/usuarios/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home (request):
     return render(request, "home/index.html")
+
+def dashboard (request):
+    return render(request, "dashboard/dashboard.html")
